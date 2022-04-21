3d4c58bab2eb772dcd7c90da39b79f58e9da8b50
Paweł Wróblewski <vrobel@gmail.com>
2022-04-21 16:39:26 +0200

    adding base/top Matcher and Description interfaces

---
+++ src/main/java/org/pwroblew/javapurefun/hascremt/Description.java
@@ -0,0 +1,4 @@
+package org.pwroblew.javapurefun.hascremt;
+
+public interface Description {
+}
---
+++ src/main/java/org/pwroblew/javapurefun/hascremt/Matcher.java
@@ -0,0 +1,15 @@
+package org.pwroblew.javapurefun.hascremt;
+
+import io.vavr.Tuple2;
+import java.util.function.Predicate;
+
+public interface Matcher<T> extends Predicate<T> {
+    Tuple2<Boolean, Description> match(T actual);
+
+    @Override
+    default boolean test(T t) {
+        return this.match(t)._1();
+    }
+
+    Description describe();
+}
